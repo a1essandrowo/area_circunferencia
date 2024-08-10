9a340659b2c7ca0baada178dae7c36b1865e4641
a1essandrowo <angelochamale@gmail.com>
2024-08-09 23:20:54 -0600

    se le agrega un comentario al mensaje nuevo que se agrego

--- area_circunferencia.java
+++ area_circunferencia.java
@@ -16,7 +16,7 @@ public static void main (String []args){
      * Primero obtenemos el radio y asi poder aplicar la formula 
      * agregamos "double" para cantidades con punto decimal :)
      */
-    System.out.println("Hola usuario, favor de ingresar los datos solicitantes :)");
+    System.out.println("Hola usuario, favor de ingresar los datos solicitantes :)"); //agregamos un pequeño mensaje de bienvenida
     System.out.println("Ingresa el valor del radio:");
     double radio = scan.nextDouble();
 
